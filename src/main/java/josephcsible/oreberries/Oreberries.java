37f37b545046e277ec1b8680f60885f1bec3e164
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-08-25 19:35:55 -0400

    Add the name and description

--- src/main/java/josephcsible/oreberries/Oreberries.java
+++ src/main/java/josephcsible/oreberries/Oreberries.java
@@ -1,15 +1,15 @@
-package com.example.examplemod;
+package josephcsible.oreberries;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = Oreberries.MODID, version = Oreberries.VERSION)
+public class Oreberries
 {
-    public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "oreberries";
+    public static final String VERSION = "0.1.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
