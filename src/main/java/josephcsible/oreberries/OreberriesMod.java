3cf7e6325397b98a4d4bf38130f0c56b14ebf139
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-09-10 00:57:20 -0400

    Require Forge 2348

--- src/main/java/josephcsible/oreberries/OreberriesMod.java
+++ src/main/java/josephcsible/oreberries/OreberriesMod.java
@@ -29,7 +29,14 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = OreberriesMod.MODID, name = "Oreberries", version = OreberriesMod.VERSION, acceptedMinecraftVersions = "[1.12, 1.13)", useMetadata = true)
+@Mod(
+	modid = OreberriesMod.MODID,
+	name = "Oreberries",
+	version = OreberriesMod.VERSION,
+	acceptedMinecraftVersions = "[1.12, 1.13)",
+	dependencies = "after:forge@[14.21.0.2348,)",
+	useMetadata = true
+)
 public class OreberriesMod
 {
     public static final String MODID = "oreberries";
