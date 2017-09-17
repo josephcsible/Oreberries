e3737597f4edf8bdbbc97f782cd9b621b141f618
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-09-17 16:22:04 -0400

    Bump version

--- src/main/java/josephcsible/oreberries/OreberriesMod.java
+++ src/main/java/josephcsible/oreberries/OreberriesMod.java
@@ -40,7 +40,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 public class OreberriesMod
 {
 	public static final String MODID = "oreberries";
-	public static final String VERSION = "0.2.0";
+	public static final String VERSION = "0.3.0";
 
 	public static Logger logger;
 
